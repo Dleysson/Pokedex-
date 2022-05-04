52231a4e55407935341a2417c509c97bf04c30b7
Dleysson <dleysson9@gmail.com>
2022-05-04 20:33:57 -0300

    feat: pokemon infos on home page
    addition of coin flip effect to display pokemon infos

--- Pokedex/src/app/home/home.component.ts
+++ Pokedex/src/app/home/home.component.ts
@@ -17,7 +17,36 @@ export class HomeComponent implements OnInit {
     this.fillHome(151)
   }
 
-  private async fillHome(qty: number) {
+  public showPokemonInfo(event: Event): void {
+    const target = (<HTMLLIElement>event.target);
+    const pokemonInfo = (<HTMLDivElement>(target).childNodes[1]);
+    const pokemonImg = (<HTMLDivElement>(target).childNodes[0]);
+    // Rotate effect
+    pokemonImg.style.transform = 'rotateY(90deg)';
+    // Show info
+    setTimeout(() => {
+      pokemonInfo.style.opacity = '1';
+      pokemonInfo.style.transform = 'rotateY(360deg)';
+    }, 500);
+  }
+
+
+  public hidePokemonInfo(event: Event): void {
+    const target = (<HTMLLIElement>event.target);
+    const pokemonInfo = (<HTMLDivElement>(target).childNodes[1])
+    const pokemonImg = (<HTMLDivElement>(target).childNodes[0])
+    // Hide info
+    setTimeout(() => {
+      pokemonInfo.style.transform = '';
+      pokemonInfo.style.opacity = '';
+    }, 500);
+    // Rotate effect
+    setTimeout(() => {
+      pokemonImg.style.transform = ''
+    }, 1000);
+  }
+
+  private async fillHome(qty: number): Promise<void> {
     let id = 1
     for (id = 1; id < qty; id++) {
       await this.pokemonService.getPokemonsById(id).toPromise()
